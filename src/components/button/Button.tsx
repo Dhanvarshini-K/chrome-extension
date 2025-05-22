f0ee05a8ca468233160b75e097c21558e79ea4fa
Dhanvarshini <dhanvarshini.k@numentica-ui.com>
2025-05-22 15:23:43 +0530

    Refactor button container

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,15 +4,19 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
   buttonText?: string;
   className?: string;
   onClick?: () => void;
+  disabled?: boolean;
   children?: React.ReactNode;
 }
 
-const Button = ({ buttonText, className, onClick, children }: ButtonProps) => {
+
+const Button = ({ buttonText, className, onClick, disabled,children }: ButtonProps) => {
+
   return (
     <button 
       type="button" 
-      className={className ? className : "button-container"} 
+      className={`button-container ${className ? className : ""}`} 
       onClick={onClick}
+      disabled = {disabled}
     >
       {children || buttonText}
     </button>
